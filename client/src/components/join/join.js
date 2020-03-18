4a3768049cfbe321206e8abc2be136e994553c09
Jamie <Jamiekaram16@gmail.com>
2020-03-17 23:24:36 -0400

    add useEffect

---
+++ client/src/components/join/join.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+       <div>
+           <div>
+               <h1>Join</h1>
+               <div><input placeholder="Name" type="text" onChange={(event) => setName(event.target.value)} /></div>
+               <div><input placeholder="Room" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&${room}`}>
+                 <button type="submit" >sign In</button>
+                </Link>
+           </div>
+       </div>
+    )
+}
+
+export default Join;
